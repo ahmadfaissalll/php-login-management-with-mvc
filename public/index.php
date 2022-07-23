2f65fbcd6b0d5345dbe4504f7e75bfa7e8582971
Ahmad Faisal <81705914+ahmadfaissalll@users.noreply.github.com>
2022-07-23 08:22:44 +0700

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use AhmadFaisal\Belajar\PHP\MVC\App\Router;
+use AhmadFaisal\Belajar\PHP\MVC\Controller\HomeController;
+use AhmadFaisal\Belajar\PHP\MVC\Controller\ProductController;
+use AhmadFaisal\Belajar\PHP\MVC\Middleware\AuthMiddleware;
+
+Router::add('GET', '/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)', ProductController::class, 'categories');
+
+Router::add("GET", '/', HomeController::class, 'index');
+Router::add("GET", '/hello', HomeController::class, 'hello', [AuthMiddleware::class]);
+Router::add("GET", '/world', HomeController::class, 'world', [AuthMiddleware::class]);
+Router::add("GET", '/about', HomeController::class, 'about');
+
+Router::run();
