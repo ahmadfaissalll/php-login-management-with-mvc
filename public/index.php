c23c38cf3df97ded8164b99feb3f8e840d6825f2
Ahmad Faisal <81705914+ahmadfaissalll@users.noreply.github.com>
2022-07-23 19:08:12 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,15 +3,27 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use AhmadFaisal\Belajar\PHP\MVC\App\Router;
+use AhmadFaisal\Belajar\PHP\MVC\Config\Database;
 use AhmadFaisal\Belajar\PHP\MVC\Controller\HomeController;
-use AhmadFaisal\Belajar\PHP\MVC\Controller\ProductController;
-use AhmadFaisal\Belajar\PHP\MVC\Middleware\AuthMiddleware;
+use AhmadFaisal\Belajar\PHP\MVC\Controller\UserController;
+use AhmadFaisal\Belajar\PHP\MVC\Middleware\MustNotLoginMiddleware;
+use AhmadFaisal\Belajar\PHP\MVC\Middleware\MustLoginMiddleware;
+use PHPUnit\Framework\MockObject\DuplicateMethodException;
 
-Router::add('GET', '/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)', ProductController::class, 'categories');
+Database::getConnection('prod');
 
-Router::add("GET", '/', HomeController::class, 'index');
-Router::add("GET", '/hello', HomeController::class, 'hello', [AuthMiddleware::class]);
-Router::add("GET", '/world', HomeController::class, 'world', [AuthMiddleware::class]);
-Router::add("GET", '/about', HomeController::class, 'about');
+// Home Controller
+Router::add("GET", '/', HomeController::class, 'index', []);
+
+// User Controller
+Router::add("GET", '/users/register', UserController::class, 'register', [MustNotLoginMiddleware::class]);
+Router::add("POST", '/users/register', UserController::class, 'postRegister', [MustNotLoginMiddleware::class]);
+Router::add("GET", '/users/login', UserController::class, 'login', [MustNotLoginMiddleware::class]);
+Router::add("POST", '/users/login', UserController::class, 'postLogin', [MustNotLoginMiddleware::class]);
+Router::add("GET", '/users/logout', UserController::class, 'logout', [MustLoginMiddleware::class]);
+Router::add("GET", '/users/profile', UserController::class, 'updateProfile', [MustLoginMiddleware::class]);
+Router::add("POST", '/users/profile', UserController::class, 'postUpdateProfile', [MustLoginMiddleware::class]);
+Router::add("GET", '/users/password', UserController::class, 'updatePassword', [MustLoginMiddleware::class]);
+Router::add("POST", '/users/password', UserController::class, 'postUpdatePassword', [MustLoginMiddleware::class]);
 
 Router::run();
